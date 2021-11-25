3949a61c3ad31b73abe96c795de5c8681479bd5f
Diego Sousa <diego@diegosousa.com.br>
2021-11-25 01:16:41 -0300

    Post Controller Index

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,18 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'post')]
-    public function index(): Response
+    public function index(Request $req): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PostController.php',
-        ]);
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+        return $this->json($posts);
     }
 }
