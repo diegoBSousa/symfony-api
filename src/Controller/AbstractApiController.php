5c082a932e6da85b8ab6636ef0d47b9d51cba404
Diego Sousa <diego@diegosousa.com.br>
2021-11-25 02:33:39 -0300

    Declares return type for buildForm

--- src/Controller/AbstractApiController.php
+++ src/Controller/AbstractApiController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as ControllerAbstractController;
+use Symfony\Component\Form\FormInterface;
 
 abstract class AbstractController extends ControllerAbstractController
 {
-  protected function buildForm(string $type, $data = null, array $options = [])
-  {
+  protected function buildForm(
+    string $type,
+    $data = null,
+    array $options = []
+  ): FormInterface {
     $options = array_merge($options, [
       'csrf_protection' => false,
     ]);
