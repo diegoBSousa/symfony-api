71a8048896a807fa42e6f5a508a2eb0f119fafee
Diego Sousa <diego@diegosousa.com.br>
2021-11-25 09:55:02 -0300

    Create a new tag

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Tag;
+use App\Form\TagType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class TagController extends AbstractApiController
@@ -11,5 +13,24 @@ class TagController extends AbstractApiController
     {
         $tags = $this->getDoctrine()->getRepository(Tag::class)->findAll();
         return $this->response($tags);
+    }
+
+    public function create(Request $res): Response
+    {
+        $form = $this->buildForm(TagType::class);
+
+        $form->handleRequest($res);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->response($form, Response::HTTP_BAD_REQUEST);
+        }
+
+        /** @var Tag $tag */
+        $tag = $form->getData();
+
+        $this->getDoctrine()->getManager()->persist($tag);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->response($tag);
     }
 }
