b248779d0b7a0e780c84da0005efcad38d2e583e
Diego Sousa <diego@diegosousa.com.br>
2021-11-25 09:37:26 -0300

    Set contraints

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,17 +4,52 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Url;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('image')
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    'constraints' => [
+                        new NotNull([
+                            'message' => 'Title cannot be empty!'
+                        ]),
+                    ]
+                ]
+            )
+            ->add(
+                'content',
+                TextType::class,
+                [
+                    'constraints' => [
+                        new NotNull([
+                            'message' => 'Content cannot be empty!'
+                        ]),
+                    ]
+                ]
+            )
+            ->add(
+                'image',
+                UrlType::class,
+                [
+                    'constraints' => [
+                        new NotNull([
+                            'message' => 'Image cannot be empty!'
+                        ]),
+                        new Url()
+                    ]
+                ]
+            )
             ->add('tags');
     }
 
