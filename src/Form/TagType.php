76e3e8483ac1813fc5c5f9cb94a8bd527a1d76ab
Diego Sousa <diego@diegosousa.com.br>
2021-11-25 09:43:44 -0300

    Set constraints for TagType

--- src/Form/TagType.php
+++ src/Form/TagType.php
@@ -4,17 +4,28 @@ namespace App\Form;
 
 use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class TagType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('tag')
-            ->add('posts')
-        ;
+            ->add(
+                'tag',
+                TextType::class,
+                [
+                    'constraints' => [
+                        new NotNull([
+                            'message' => 'Tag cannot be empty.'
+                        ])
+                    ]
+                ]
+            )
+            ->add('posts');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
